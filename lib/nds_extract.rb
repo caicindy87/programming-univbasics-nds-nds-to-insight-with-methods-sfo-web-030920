11fb5548128c9f697f75dff7ace1c9895d1b1ebd
Cindy Cai <caicindy87@gmail.com>
2020-03-02 19:02:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,6 +19,17 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
+  def gross_for_director(director_data)
+  gross_sum = 0
+  i = 0
+  movies_array_length = directors_database[0][:movies]
+  while i < movies_array_length.length do
+  gross_sum += movies_array_length[i][:worldwide_gross]
+  i += 1
+end
+gross_sum
+end
+
   result = {}
   result[directors_database[]]
   nil
